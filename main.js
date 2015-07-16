54d2f3dd02dc23e308bd8e803e1bf3b745796ace
Darin Jacobson <djake1680@gmail.com>
2015-07-15 22:27:11 -0700

    version .05.3 [in-progress]

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-function hide_card(card_back) {
-	$(card_back).css("display", "none");
-		console.log("This worked");
+function hide_card(card_back) { 
+	//changes id that was passed to the function into a variable
+	$(card_back).css("display", "none"); 
+		//uses variable from function to add css into its' id's code
+		//console log below determines what card was clicked by looking at the variable that passed through the function
+		console.log(card_back + " was clicked");
 }
